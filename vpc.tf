d0ae77c4af4c1a569e6b2af773ac221bb4fea5a2
vikasghandge <ghandgevikas804@gmail.com>
2024-08-22 00:00:57 +0530

    --- Auto Git Commit ---

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "my-vpc" {
 }
 
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.public.id 
+    vpc_id = aws_vpc.my_
     cidr_block = 192.168.0.0/24
     tags = {
         name = "my_vpc"
