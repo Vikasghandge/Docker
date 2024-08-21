bdcc8454f0d8eab11efefd92506575aed164d0ca
vikasghandge <ghandgevikas804@gmail.com>
2024-08-21 23:51:09 +0530

    --- Auto Git Commit ---

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
 provider "aws" {
     region = "ap-south-1"
 }
-
-resource "aws_instance" "server_1"{
-    ami = "ami-0ad21ae1d0696ad58"
-    instance_type = "t2.micro"
-    key_name = "devops-key"
-    tags = {
-        name = "my_ec2"
-    }
-
-}
