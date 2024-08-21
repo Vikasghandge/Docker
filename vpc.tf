7768a273b4a1e3c52b33c93f9598a77273f63a3a
vikasghandge <ghandgevikas804@gmail.com>
2024-08-21 23:56:10 +0530

    --- Auto Git Commit ---

--- vpc.tf
+++ vpc.tf
@@ -14,4 +14,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
     vpc_id = vpc_id.private.vpc_id
     cidr_block = 192.168.1.0/24
+    tags = {
+        name = "my-private"
+    }
 }
