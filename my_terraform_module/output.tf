9f32eaa0673e1a4837ca013ddc5b403a0736c206
vikasghandge <ghandgevikas804@gmail.com>
2024-08-22 14:27:40 +0530

    --- Auto Git Commit ---

--- my_terraform_module/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-    description = " ec2 id"
-    value = "aws_instance.example.id"
-}
-
-output "public_ip" {
-    description = "the ip of public instance"
-    value = "aws_instance.example.public_ip"
-}
