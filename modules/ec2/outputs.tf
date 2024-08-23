dc7fe65802025133cb63deba089f223315416399
vikasghandge <ghandgevikas804@gmail.com>
2024-08-23 18:07:36 +0530

    --- Auto Git Commit ---

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,7 +1,7 @@
 # modules/ec2/outputs.tf
 output "instance_id" {
   description = "The ID of the EC2 instance."
-  value       = aws_instance.this.id
+  value       = aws_instance.this_instance.id
 }
 
 output "public_ip" {
