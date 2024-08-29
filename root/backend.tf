f7b0b605d41dc0169a0b5fbaf43062230447f39f
vikasghandge <ghandgevikas804@gmail.com>
2024-08-29 18:20:16 +0530

    --- Auto Git Commit ---

--- root/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "tfstate-piyush-101"
-    key    = "backend/10weeksofcloudops-demo.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "remote-backend"
-  }
-}
--- root/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-project_name = "10weeksofcloudops-demo"
-region = "ap-south-1"
-
