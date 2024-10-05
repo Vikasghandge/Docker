ae5a3e6bb1ff4358189d559e8446b8f1be8341bd
vikasghandge <ghandgevikas804@gmail.com>
2024-10-05 17:15:13 +0530

    --- Auto Git Commit ---

--- b37.groovy
+++ b37.groovy
@@ -4,6 +4,9 @@ pipeline {
     environment {
         DOCKER_IMAGE = 'ghandgevikas/my-docker-repo:b37'
         CONTAINER_NAME = 'container-1'
+        SONAR_TOKEN = 'sqa_b9ff5dfd6c386a3b4b79e391c4e8e8d0937f6f1d'  // Your SonarQube token
+        SONAR_HOST_URL = 'http://13.201.39.7:9000'  // Your SonarQube server URL
+        SONAR_PROJECT_KEY = 'your_project_key' // Replace with your actual SonarQube project key
     }
 
     stages {
@@ -29,6 +32,22 @@ pipeline {
             }
         }
 
+        stage('SonarQube Analysis') {
+            steps {
+                script {
+                    echo 'Running SonarQube analysis...'
+                    // You may need to mount the code directory for analysis
+                    sh """
+                    sonar-scanner \
+                    -Dsonar.projectKey=${SONAR_PROJECT_KEY} \
+                    -Dsonar.sources=. \
+                    -Dsonar.host.url=${SONAR_HOST_URL} \
+                    -Dsonar.login=${SONAR_TOKEN}
+                    """
+                }
+            }
+        }
+
         stage('Deploy Docker Image') {
             steps {
                 script {
@@ -39,6 +58,7 @@ pipeline {
             }
         }
 
+        // Uncomment to include a cleanup stage
         // stage('Cleanup') {
         //     steps {
         //         script {
