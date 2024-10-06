75faa2ab514c50e70f6b801650a1ff85ccb25753
Vikasghandge <ghandgevikas804@gmail.com>
2024-10-06 13:56:38 +0530

    Update b38.groovy

--- b38.groovy
+++ b38.groovy
@@ -3,20 +3,25 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                // This stage is for building your project
-                sh 'mvn clean verify'
+                // Change to the directory containing the pom.xml file
+                dir('my-project') {
+                    // This stage is for building your project
+                    sh 'mvn clean verify'
+                }
             }
         }
         stage('SonarQube Analysis') {
             steps {
                 // SonarQube analysis using Maven
                 withCredentials([string(credentialsId: 'sonarqube-token', variable: 'SONAR_TOKEN')]) {
-                    sh """
-                    mvn sonar:sonar \
-                      -Dsonar.projectKey=my-project-key \
-                      -Dsonar.host.url=http://13.235.33.149:9000 \
-                      -Dsonar.login=${SONAR_TOKEN}
-                    """
+                    dir('my-project') { // Ensure we are in the correct directory for analysis
+                        sh """
+                        mvn sonar:sonar \
+                          -Dsonar.projectKey=my-project-key \
+                          -Dsonar.host.url=http://13.235.33.149:9000 \
+                          -Dsonar.login=${SONAR_TOKEN}
+                        """
+                    }
                 }
             }
         }
