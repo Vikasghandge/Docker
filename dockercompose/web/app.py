3a5a9096c85661313cb8fc221f489ab8b90eb410
vikasghandge <ghandgevikas804@gmail.com>
2024-11-06 11:18:34 +0530

    --- Auto Git Commit ---

--- dockercompose/web/app.py
+++
@@ -1,1 +0,0 @@
-echo "from flask import Flask\napp = Flask(__name__)\n@app.route('/')\ndef home():\n    return 'Hello from the web service!'\nif __name__ == '__main__':\n    app.run(host='0.0.0.0')" > web/app.py
