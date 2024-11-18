e0e94fd51a92f1ea9380d671701f06e5ceff5b0f
vikasghandge <ghandgevikas804@gmail.com>
2024-11-18 16:05:09 +0530

    --- Auto Git Commit ---

---
+++ season_switcher/b40.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Clone the repository
+                git branch: 'main', url: 'https://github.com/Vikasghandge/Docker.git'
+            }
+        }
+
+        stage('Docker Build') {
+            steps {
+                script {
+                    // Specify the path to the Dockerfile
+                    sh 'docker build -t season_switcher_image ./season_switcher'
+                }
+            }
+        }
+    }
+}
