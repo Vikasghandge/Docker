a4bb7b9dd7afd60e30d1f2ad36fc0b2b1986f9a1
vikasghandge <ghandgevikas804@gmail.com>
2024-11-18 16:37:38 +0530

    --- Auto Git Commit ---

--- b40.groovy
+++ b40.groovy
@@ -2,9 +2,27 @@ pipeline {
     agent any
 
     stages {
-        stage('Hello') {
+        stage('Checkout') {
             steps {
-                echo 'Hello World'
+                git branch: 'main', url: 'https://github.com/Vikasghandge/Docker.git'
+            }
+        }
+
+        stage('Docker Build') {
+            steps {
+                sh 'docker build -t season_switcher_image ./season_switcher'
+            }
+        }
+
+        stage('Docker Push') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+                    sh '''
+                        echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                        docker tag season_switcher_image $DOCKER_USER/season_switcher_image:latest
+                        docker push $DOCKER_USER/season_switcher_image:latest
+                    '''
+                }
             }
         }
     }
