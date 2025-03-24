be93ced1652301a9380f40264ba40c7424d1b253
vikasghandge <ghandgevikas804@gmail.com>
2025-03-24 23:46:34 +0530

    --- Auto Git Commit ---

---
+++ last/todayyy.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"  # Correct AWS region (Mumbai)
+}
+
+resource "aws_instance" "my_new_instance" {  # Fixed resource name
+  ami           = "ami-0e35ddab05955cf57"  # Ensure this AMI exists in ap-south-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
