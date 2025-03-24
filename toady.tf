8e0d85a6c3d2644f9a13ee4fedd0e7108ca0bf7c
vikasghandge <ghandgevikas804@gmail.com>
2025-03-24 23:42:17 +0530

    --- Auto Git Commit ---

--- toady.tf
+++ toady.tf
@@ -6,3 +6,17 @@ resource "aws_instance" "my_new_onstance" {
     instance_type = "t2.micro"
     
 }
+
+
+provider "aws" {
+  region = "ap-south-1"  # Correct AWS region (Mumbai)
+}
+
+resource "aws_instance" "my_new_instance" {  # Fixed resource name
+  ami           = "ami-0e35ddab05955cf57"  # Ensure this AMI exists in ap-south-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
