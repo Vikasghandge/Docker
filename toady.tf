b01a6c494251960b4b2a77f2f33f47e7d1af0b75
vikasghandge <ghandgevikas804@gmail.com>
2025-03-24 23:43:17 +0530

    --- Auto Git Commit ---

--- toady.tf
+++ toady.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-    region = "ap-south-1"
-}
-resource "aws_instance" "my_new_onstance" {
-    ami = "ami-0e35ddab05955cf57"
-    instance_type = "t2.micro"
+# provider "aws" {
+#     region = "ap-south-1"
+# }
+# resource "aws_instance" "my_new_onstance" {
+#     ami = "ami-0e35ddab05955cf57"
+#     instance_type = "t2.micro"
     
-}
+# }
 
 
 provider "aws" {
