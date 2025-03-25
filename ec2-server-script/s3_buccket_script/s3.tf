4430322bafc3c8ca46a0cae8b37306baf36bb197
vikasghandge <ghandgevikas804@gmail.com>
2025-03-25 10:29:18 +0530

    --- Auto Git Commit ---

--- ec2-server-script/s3_buccket_script/s3.tf
+++ ec2-server-script/s3_buccket_script/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-buckettt"
 
   tags = {
-    Name        = "My bucketttt-"
+    Name        = "My bucketttt-vikaspatil"
     Environment = "Dev"
   }
 }
