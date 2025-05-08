42e39d5ae4144f856e4da572f3fac9a76fa55699
vikasghandge <ghandgevikas804@gmail.com>
2025-05-08 18:26:14 +0530

    --- Auto Git Commit ---

--- examp.groovy
+++ examp.groovy
@@ -4,7 +4,7 @@ pipeline {
     environment {
         DOCKER_IMAGE = "ghandgevikas/devopsexamapp:latest"
         SCANNER_HOME = tool 'sonar-scanner'
-        CLUSTER_A
+        CLUSTER_NAME = ""
     }
 
     stages {
